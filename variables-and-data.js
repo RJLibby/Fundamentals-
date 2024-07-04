86d9ab84ec11baebd7c94f501e178672aff2d091
RJLibby <rlibby@cnm.edu>
2024-07-03 23:27:54 -0600

    Add files via upload

---
+++ variables-and-data.js
@@ -0,0 +1,25 @@
+/*
+* Variables, Data Types, and Typing
+ */
+let wordData;
+wordData = "Word Data"
+
+console.log(wordData);
+
+let subTotal = 100;
+let tax = subTotal * 0.07;
+let totalWithTax = tax + subTotal;
+
+
+let obj ={
+    key1: "value",
+    key2: 4,
+    boolean: true,
+    obj2: {
+        obj2key: "value of inner object",
+    }
+};
+
+console. log(obj.obj2.obj2key1);
+
+
